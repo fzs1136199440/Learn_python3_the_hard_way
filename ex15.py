ede15b9824ca29404d591227276011508263d56b
JerryFu <fzs1136199440@icloud.com>
2020-03-28 18:36:06 +0800

    Create ex15.py

---
+++ ex15.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+script,  filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+txt.close()
+
+print("Type the filename again:")
+# file_again = input(">>> ")
+
+txt_again = open(input(">>> "))
+
+print(txt_again.read())
+txt_again.close()
