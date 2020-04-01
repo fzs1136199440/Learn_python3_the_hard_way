ca4c4faf72f645877de22e2d82e6dd4b3adaf8e7
JerryFu <fzs1136199440@icloud.com>
2020-04-01 16:49:58 +0800

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -7,18 +7,22 @@ script, from_file, to_file = argv
 
 # we could do these two on one line, how?
 # we can do this: indata = open(from_file).read()
-
-indata = open(from_file).read()
+# in_file = open(from_file)
+# indata = in_file.read()
+# indata = open(from_file).read()
 
 # print(f"The input file is {len(indata)} bytes long.")
 
 print(f"Does the output file exist? {exists(to_file)}.")
-print("Ready, hit RETURN to continue, CTRL-C to abort.")
-input()
+# print("Ready, hit RETURN to continue, CTRL-C to abort.")
+# input()
+out_file = open(to_file, 'w')
+out_file.write(open(from_file).read())
+# open(to_file, 'w').write(open(from_file).read())
 
-open(to_file, 'w').write(indata)
+# print("Alright, all done.")
 
-print("Alright, all done.")
-
-open(to_file, 'w').close()
+# in_file.close()
 open(from_file).close()
+out_file.close()
+# open(to_file, 'w').close()
